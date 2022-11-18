9000bc5b9f2c898cd54b9a28ecc5f2d544a82ddd
Matt Schoen <schoen@unity3d.com>
2022-11-18 13:20:21 -0800

    Fix native collection memory leak in ARCoreAnalytics

--- Editor/Scripts/Internal/ARCoreAnalytics.cs
+++ Editor/Scripts/Internal/ARCoreAnalytics.cs
@@ -128,6 +128,7 @@ namespace Google.XR.ARCoreExtensions.Editor.Internal
             {
                 if (_webRequest.isDone == true)
                 {
+                    _webRequest.uploadHandler.Dispose();
                     _webRequest = null;
                 }
             }
